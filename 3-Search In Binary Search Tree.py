2c2ff4d5aefdd07c47775cf4f7f6d7e693a0181e
ruinanzhang <rz2363@columbia.edu>
2020-04-18 02:56:36 -0400

    Renamed files and added q3:52. Search In Binary Search Tree

---
+++ 3-Search In Binary Search Tree.py
@@ -0,0 +1,32 @@
+# Tag : Binary Search Tree
+
+# -----------------------------------------------------------------------------------
+# Description:
+# Find the target key K in the given binary search tree, 
+# return the node that contains the key if K is found, otherwise return null.
+
+# -----------------------------------------------------------------------------------
+# Assumptions:
+# There are no duplicate keys in the binary search tree
+
+#  -----------------------------------------------------------------------------------
+# /**
+#  * public class TreeNode {
+#  *   public int key;
+#  *   public TreeNode left;
+#  *   public TreeNode right;
+#  *   public TreeNode(int key) {
+#  *     this.key = key;
+#  *   }
+#  * }
+#  */
+
+
+# *************************************************************************************
+# Solution: 
+
+public class Solution {
+  public TreeNode search(TreeNode root, int key) {
+    // Write your solution here
+  }
+}
